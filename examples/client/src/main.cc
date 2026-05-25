f02fa7fe05af21e60e4f6302eb2f9985b54ae300
reftch <j.developer@hotmail.com>
2026-05-25 14:15:47 +0200

    Code cleanup

--- examples/client/src/main.cc
+++ examples/client/src/main.cc
@@ -1,24 +1,21 @@
-
 #include "client.h"
 #include "server.h"
 
 int main() {
     static auto& log = http::Logger::getInstance();
     http::Client cli("https://0.0.0.0:8443");
-    // http::Client cli("http://0.0.0.0:8080");
 
-    // for (size_t i = 0; i < 10; ++i) {
-    auto res = cli.Get("/api/v1/inc/3");
-    // auto res = cli.Get("/");
-    if (res && res->status() == http::Status::ok) {
-        log.Info("Response status: {}", static_cast<int>(res->status()));
-        log.Info("Response body: {}", res->content());
-        // log.Info("Response Content-Type: {}", res->headers().at("Content-Type"));
-    } else {
-        log.Error("Request failed");
-        // break;
+    for (size_t i = 0; i < 10; ++i) {
+        auto res = cli.Get("/api/v1/inc/" + std::to_string(i));
+        if (res && res->status() == http::Status::ok) {
+            log.Info("Response status: {}", static_cast<int>(res->status()));
+            log.Info("Response body: {}", res->content());
+            log.Info("Response Content-Type: {}", res->headers().at("Content-Type"));
+        } else {
+            log.Error("Request failed");
+            break;
+        }
     }
-    // }
 
     return 0;
 }
