a6a0c4eb89a3015e8205d29c56b2ec1559b3ab91
reftch <j.developer@hotmail.com>
2026-08-11 19:28:30 +0200

    Code cleanup. Optimize docker image

--- examples/server/src/main.cc
+++ examples/server/src/main.cc
@@ -1,11 +1,9 @@
-#include <chrono>
-
-// #define HTTP_OPENSSL_SUPPORT
-// #include "sslserver.h"
+#include <csignal>
 
 #include "server.h"
 
-[[nodiscard]]
+static http::Server* server = nullptr;
+
 std::string getCurrentTimeJson() {
     // Get current time
     auto now = std::chrono::system_clock::now();
@@ -19,13 +17,21 @@ std::string getCurrentTimeJson() {
     ss << std::put_time(&local_tm, "%d.%m.%Y %H:%M:%S");
 
     return std::format(R"({{"content":"<div id='wstime'>{}</div>"}})", ss.str());
-}
+};
 
 int main() {
-    http::Server s("0.0.0.0", 8080);
-    // auto& log = http::Logger::getInstance();
-    // http::Logger::getInstance().setLevel(http::Level::DEBUG);
-    // http::SSLServer s("0.0.0.0", 8443, "cert.pem", "key.pem");
+    // http::SSLServer s("0.0.0.0", 8080, "cert.pem", "key.pem");
+    http::Server s;
+
+    server = &s;
+
+    std::signal(SIGINT, [](int) {
+        // std::cout << "Shutting down..." << std::endl;
+        if (server) {
+            server->stop();
+            std::exit(0);
+        }
+    });
 
     // REST endpoint
     s.setRoute<http::HttpMethod::GET>("/api/v1/users/:v", [](const http::Request& req, http::Response& res) {
@@ -48,14 +54,69 @@ int main() {
         });
     });
 
-    // Post request for CORS
-    // s.setPostRoute([&](const http::Request&, http::Response& res) {
-    //     res.setHeader("Access-Control-Allow-Origin", "*");
-    //     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    //     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    // });
-
     s.run();
 
     return 0;
 }
+
+// #include <chrono>
+
+// #define HTTP_OPENSSL_SUPPORT
+// #include "sslserver.h"
+
+// #include "server.h"
+
+// [[nodiscard]]
+// std::string getCurrentTimeJson() {
+//     // Get current time
+//     auto now = std::chrono::system_clock::now();
+//     std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+
+//     // Convert to local time structure
+//     std::tm local_tm = *std::localtime(&now_c);
+
+//     // Use stringstream to format the date
+//     std::stringstream ss;
+//     ss << std::put_time(&local_tm, "%d.%m.%Y %H:%M:%S");
+
+//     return std::format(R"({{"content":"<div id='wstime'>{}</div>"}})", ss.str());
+// }
+
+// int main() {
+//     http::Server s("0.0.0.0", 8080);
+//     // auto& log = http::Logger::getInstance();
+//     // http::Logger::getInstance().setLevel(http::Level::DEBUG);
+//     // http::SSLServer s("0.0.0.0", 8443, "cert.pem", "key.pem");
+
+//     // REST endpoint
+//     s.setRoute<http::HttpMethod::GET>("/api/v1/users/:v", [](const http::Request& req, http::Response& res) {
+//         res << http::ContentType::JSON << "{\"value\":\"" << req.params().at("v") << "\"}";
+//     });
+
+//     // int counter = 0;
+
+//     // // SSE handler
+//     // s.setRoute<http::HttpMethod::GET>("/stream", [&](const http::Request&, http::Response& res) {
+//     //     s.repeatEvery(res.getId(), std::chrono::seconds(1), [res, &counter]() mutable {
+//     //         return res.stream(std::format("data: {}\n\n", ++counter).c_str());
+//     //     });
+//     // });
+
+//     // // WebSocket handler
+//     // s.setRoute("/wstime", [&](http::WebSocket& ws) {
+//     //     s.repeatEvery(ws.getId(), std::chrono::seconds(1), [ws]() mutable {
+//     //         return ws.send(getCurrentTimeJson());
+//     //     });
+//     // });
+
+//     // Post request for CORS
+//     // s.setPostRoute([&](const http::Request&, http::Response& res) {
+//     //     res.setHeader("Access-Control-Allow-Origin", "*");
+//     //     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//     //     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//     // });
+
+//     s.run();
+
+//     return 0;
+// }
