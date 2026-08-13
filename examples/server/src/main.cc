9decaf5938635b6d3484efd7526967c27ff9ec3e
reftch <j.developer@hotmail.com>
2026-08-13 12:32:08 +0200

    Fixed TaskManager

--- examples/server/src/main.cc
+++ examples/server/src/main.cc
@@ -1,8 +1,9 @@
-#include <csignal>
+#include <chrono>
 
 #include "server.h"
+#include "worker.h"
 
-static http::Server* server = nullptr;
+using namespace std::chrono_literals;
 
 std::string getCurrentTimeJson() {
     // Get current time
@@ -21,17 +22,8 @@ std::string getCurrentTimeJson() {
 
 int main() {
     // http::SSLServer s("0.0.0.0", 8080, "cert.pem", "key.pem");
-    http::Server s;
-
-    server = &s;
-
-    std::signal(SIGINT, [](int) {
-        // std::cout << "Shutting down..." << std::endl;
-        if (server) {
-            server->stop();
-            std::exit(0);
-        }
-    });
+    http::Server s("127.0.0.1", 8082);
+    auto& taskManager = http::TaskManager::get_instance();
 
     // REST endpoint
     s.setRoute<http::HttpMethod::GET>("/api/v1/users/:v", [](const http::Request& req, http::Response& res) {
@@ -41,82 +33,27 @@ int main() {
     int counter = 0;
 
     // SSE handler
-    s.setRoute<http::HttpMethod::GET>("/stream", [&](const http::Request&, http::Response& res) {
-        s.repeatEvery(res.getId(), std::chrono::seconds(1), [res, &counter]() mutable {
-            return res.stream(std::format("data: SSE count: {}\n\n", ++counter).c_str());
+    s.setRoute<http::HttpMethod::GET>("/stream", [&](const http::Request& req, http::Response& res) {
+        taskManager.repeatEvery(req.path(), 50ms, [res, &counter]() mutable {
+            return res.stream(std::format("data: {}\n\n", ++counter).c_str());
         });
     });
 
     // WebSocket handler
     s.setRoute("/wstime", [&](http::WebSocket& ws) {
-        s.repeatEvery(ws.getId(), std::chrono::seconds(1), [ws]() mutable {
+        taskManager.repeatEvery("/wstime", 1s, [ws]() mutable {
+            // s.repeatEvery(ws.getId(), 1s, [ws]() mutable {
             return ws.send(getCurrentTimeJson());
         });
     });
+
+    // s.setRoute("/wstime", [&](http::WebSocket& ws) {
+    //     s.repeatEvery(ws.getId(), 1s, [ws]() mutable {
+    //         return ws.send(getCurrentTimeJson());
+    //     });
+    // });
 
     s.run();
 
     return 0;
 }
-
-// #include <chrono>
-
-// #define HTTP_OPENSSL_SUPPORT
-// #include "sslserver.h"
-
-// #include "server.h"
-
-// [[nodiscard]]
-// std::string getCurrentTimeJson() {
-//     // Get current time
-//     auto now = std::chrono::system_clock::now();
-//     std::time_t now_c = std::chrono::system_clock::to_time_t(now);
-
-//     // Convert to local time structure
-//     std::tm local_tm = *std::localtime(&now_c);
-
-//     // Use stringstream to format the date
-//     std::stringstream ss;
-//     ss << std::put_time(&local_tm, "%d.%m.%Y %H:%M:%S");
-
-//     return std::format(R"({{"content":"<div id='wstime'>{}</div>"}})", ss.str());
-// }
-
-// int main() {
-//     http::Server s("0.0.0.0", 8080);
-//     // auto& log = http::Logger::getInstance();
-//     // http::Logger::getInstance().setLevel(http::Level::DEBUG);
-//     // http::SSLServer s("0.0.0.0", 8443, "cert.pem", "key.pem");
-
-//     // REST endpoint
-//     s.setRoute<http::HttpMethod::GET>("/api/v1/users/:v", [](const http::Request& req, http::Response& res) {
-//         res << http::ContentType::JSON << "{\"value\":\"" << req.params().at("v") << "\"}";
-//     });
-
-//     // int counter = 0;
-
-//     // // SSE handler
-//     // s.setRoute<http::HttpMethod::GET>("/stream", [&](const http::Request&, http::Response& res) {
-//     //     s.repeatEvery(res.getId(), std::chrono::seconds(1), [res, &counter]() mutable {
-//     //         return res.stream(std::format("data: {}\n\n", ++counter).c_str());
-//     //     });
-//     // });
-
-//     // // WebSocket handler
-//     // s.setRoute("/wstime", [&](http::WebSocket& ws) {
-//     //     s.repeatEvery(ws.getId(), std::chrono::seconds(1), [ws]() mutable {
-//     //         return ws.send(getCurrentTimeJson());
-//     //     });
-//     // });
-
-//     // Post request for CORS
-//     // s.setPostRoute([&](const http::Request&, http::Response& res) {
-//     //     res.setHeader("Access-Control-Allow-Origin", "*");
-//     //     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//     //     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//     // });
-
-//     s.run();
-
-//     return 0;
-// }
--- src/worker.h
+++ src/worker.h
@@ -1,11 +1,46 @@
 #ifndef HTTP_TASK_H
 #define HTTP_TASK_H
 
+#include <chrono>
 #include <mutex>
 #include <thread>
 #include <unordered_map>
 
 namespace http {
+
+    class TaskManager {
+       private:
+        // Using jthread because it carries a stop_token automatically
+        std::unordered_map<std::string, std::jthread> tasks;
+        std::mutex mutex;
+
+       public:
+        static TaskManager& get_instance() {
+            static TaskManager instance;
+            return instance;
+        }
+
+        template <typename Rep, typename Period, typename F>
+        void repeatEvery(const std::string& key, std::chrono::duration<Rep, Period> interval, F&& fn) {
+            std::lock_guard<std::mutex> lock(mutex);
+            tasks.erase(key);
+
+            // Capture fn by value (moving it into the thread)
+            tasks[key] = std::jthread([interval, fn = std::forward<F>(fn)](std::stop_token stop) mutable {
+                while (!stop.stop_requested()) {
+                    // Execute the function
+                    fn();
+
+                    // Wait for the interval, but check stop_token frequently to allow instant cancellation
+                    auto next_tick = std::chrono::steady_clock::now() + interval;
+                    while (std::chrono::steady_clock::now() < next_tick) {
+                        if (stop.stop_requested()) return;
+                        std::this_thread::sleep_for(std::chrono::milliseconds(10));  // Small granularity
+                    }
+                }
+            });
+        }
+    };
 
     class Worker {
        public:
